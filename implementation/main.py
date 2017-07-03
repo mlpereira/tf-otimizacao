f785ac1b8bb712cc5cf698fe8f8bcc83fd5d57a8
Augusto Boranga <augusto.boranga@corp.globo.com>
2017-07-03 17:20:36 -0300

    lê os itens do .dat de entrada

--- implementation/main.py
+++ implementation/main.py
@@ -1,8 +1,27 @@
-import numpy as np
+import numpy
 import sys
+import re
 
+# VARS
+items = []
+restrictions = []
+
+# read filename from input
 filename = sys.argv[1]
 
-for line in open(filename, 'r'):
-    item = line.rstrip() # strip off newline and any other trailing whitespace
-    ...
+with open(filename) as f:
+    lines = f.readlines()
+
+number_of_items = int(re.findall(r'\d+', lines[0])[0])
+max_weight = int(re.findall(r'\d+', lines[1])[0])
+
+# read items
+
+i = 3
+while(lines[i] != ';\n'):
+    regex_resut = re.findall(r'\d+', lines[i])
+    item = (regex_resut[0], regex_resut[1], regex_resut[2])
+    items.append(item)
+
+    i = i + 1
+
