0825c1d4603bd242751ce64448e88e8025e2d0e8
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-07-03 19:13:59 -0300

    Cria solucao inicial

--- implementation/main.py
+++ implementation/main.py
@@ -2,6 +2,10 @@ import numpy
 import sys
 import re
 
+NUM_ITEM = 0
+PROFIT = 1
+WEIGHT = 2
+
 # read filename from input
 filename = sys.argv[1]
 
@@ -14,6 +18,8 @@ max_weight = int(re.findall(r'\d+', lines[1])[0])
 
 items = []
 restrictions = numpy.zeros(shape=(number_of_items, number_of_items))
+knapsack = []
+knapsack_weight = 0
 
 
 # read items
@@ -21,7 +27,7 @@ restrictions = numpy.zeros(shape=(number_of_items, number_of_items))
 i = 3
 while(lines[i] != ';\n'):
     regex_result = re.findall(r'\d+', lines[i])
-    item = (regex_result[0], regex_result[1], regex_result[2])
+    item = (int(regex_result[0]), int(regex_result[1]), int(regex_result[2]))
     items.append(item)
 
     i = i + 1
@@ -30,9 +36,8 @@ while(lines[i] != ';\n'):
 i = i + 3
 
 while(lines[i] != ';\n'):
-    print lines[i]
     regex_result = re.findall(r'\d+', lines[i])
-    # print regex_result
+
     line = int(regex_result[0])
     column = int(regex_result[1])
 
@@ -41,4 +46,11 @@ while(lines[i] != ';\n'):
 
     i = i + 1
 
-print restrictions
+items.sort(key=lambda (a,b,c): float(b)/c, reverse=True)
+
+for i in range(0,number_of_items):
+    if (max_weight - knapsack_weight) < items[-1][WEIGHT]:
+        break
+    if items[i][WEIGHT] < (max_weight - knapsack_weight):
+        knapsack.append(items[i])
+        knapsack_weight += items[i][WEIGHT]
