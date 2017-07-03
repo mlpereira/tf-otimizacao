58d5d28640a2cfd6ba062775bd5f2a540eebed99
Augusto Boranga <augusto.boranga@corp.globo.com>
2017-07-03 17:33:12 -0300

    lê toda a entrada

--- implementation/main.py
+++ implementation/main.py
@@ -2,26 +2,43 @@ import numpy
 import sys
 import re
 
-# VARS
-items = []
-restrictions = []
-
 # read filename from input
 filename = sys.argv[1]
 
 with open(filename) as f:
     lines = f.readlines()
 
+# VARS
 number_of_items = int(re.findall(r'\d+', lines[0])[0])
 max_weight = int(re.findall(r'\d+', lines[1])[0])
+
+items = []
+restrictions = numpy.zeros(shape=(number_of_items, number_of_items))
+
 
 # read items
 
 i = 3
 while(lines[i] != ';\n'):
-    regex_resut = re.findall(r'\d+', lines[i])
-    item = (regex_resut[0], regex_resut[1], regex_resut[2])
+    regex_result = re.findall(r'\d+', lines[i])
+    item = (regex_result[0], regex_result[1], regex_result[2])
     items.append(item)
 
     i = i + 1
 
+# read restrictions
+i = i + 3
+
+while(lines[i] != ';\n'):
+    print lines[i]
+    regex_result = re.findall(r'\d+', lines[i])
+    # print regex_result
+    line = int(regex_result[0])
+    column = int(regex_result[1])
+
+    restrictions[line][column] = 1
+    restrictions[column][line] = 1
+
+    i = i + 1
+
+print restrictions
