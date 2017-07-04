d9d464d70b423fe87c73b8d14be7e49e34cdaa3c
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-07-04 20:32:37 -0300

    Muda forma de import

--- implementation/main.py
+++ implementation/main.py
@@ -1,7 +1,7 @@
 import numpy
 import sys
 import re
-import vns
+from vns import *
 
 # constants
 
@@ -62,17 +62,17 @@ items.sort(key=lambda (a,b,c): float(b)/c, reverse=True)
 for i in range(0,number_of_items):
     if (max_weight - knapsack_weight) < items[-1][WEIGHT]:
         break
-    if vns.knapsack_has_space(max_weight, knapsack_weight, items[i]):
-        if vns.item_has_conflict(knapsack, items[i], restrictions) == False:
+    if knapsack_has_space(max_weight, knapsack_weight, items[i]):
+        if item_has_conflict(knapsack, items[i], restrictions) == False:
             knapsack.append(items[i])
             knapsack_weight += items[i][WEIGHT]
 
 
 # execute search algorithm with vns meta-heuristic
 
-N = [vns.neighborhood0, vns.neighborhood1, vns.neighborhood2]
+N = [neighborhood0, neighborhood1, neighborhood2]
 
 for i in range(0, MAX_NUMBER_OF_ITERATIONS):
     for k in range(0, NUMBER_OF_NEIGHBORHOODS):
         neighborhood = N[k](knapsack)
-        possible_solution = vns.random_neighbor(neighborhood)
+        possible_solution = random_neighbor(neighborhood)
