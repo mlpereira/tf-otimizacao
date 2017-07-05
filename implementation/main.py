fe0994da7390390ad0eb6ace748f9136d4ba54f8
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-07-05 10:58:11 -0300

    Limpa e comenta a implementacao

--- implementation/main.py
+++ implementation/main.py
@@ -3,17 +3,18 @@ import sys
 import re
 from vns import *
 
-# constants
-
+# constantes
 NUM_ITEM = 0
 PROFIT = 1
 WEIGHT = 2
 
+# numero de iteracoes
 MAX_NUMBER_OF_ITERATIONS = 10
+
+# numero de vizinhancas
 NUMBER_OF_NEIGHBORHOODS = 3
 
-# read filename from input
-
+# leitura dos dados
 filename = sys.argv[1]
 
 with open(filename) as f:
@@ -25,8 +26,6 @@ def find_line_with_numbers(line_num):
     while len(re.findall(r'\d+', lines[line_num])) == 0:
         line_num += 1
     return line_num
-
-# vars
 
 line_num = find_line_with_numbers(line_num)
 
@@ -42,8 +41,7 @@ restrictions = numpy.zeros(shape=(number_of_items, number_of_items))
 knapsack = []
 knapsack_weight = 0
 
-# read items
-
+# leitura dos dados - itens
 line_num += 1
 line_num = find_line_with_numbers(line_num)
 
@@ -54,8 +52,7 @@ while(lines[line_num] != ';\n'):
 
     line_num += 1
 
-# read restrictions
-
+# leitura dos dados - retricoes
 line_num = find_line_with_numbers(line_num)
 
 while(lines[line_num] != ';\n'):
@@ -69,9 +66,8 @@ while(lines[line_num] != ';\n'):
 
     line_num += 1
 
-
-# generate initial solution
-
+# geracao da solucao inicial
+# ordena os itens por custo beneficio
 items.sort(key=lambda (a,b,c): float(b)/c, reverse=True)
 
 for i in range(0,number_of_items):
@@ -82,49 +78,30 @@ for i in range(0,number_of_items):
             knapsack.append(items[i])
             knapsack_weight += items[i][WEIGHT]
 
-# print_knapsack(knapsack)
 
-# execute search algorithm with vns meta-heuristic
-
-N = [neighborhood0, neighborhood1, neighborhood2]
-#print evaluate_solution(knapsack)
-
-# print("1111111111111")
-#print knapsack_id_sum(knapsack)
-
-random_item_max_weight(items, 300)
-
+# execucao o VNS
 for i in range(0, MAX_NUMBER_OF_ITERATIONS):
+    print "i = " + str(i)
     k = 1
+    # laco por todas vizinhancas
     while (k <= NUMBER_OF_NEIGHBORHOODS):
-        # print str(i) + " - " + str(k)
-        # print("2222222222222")
-        # print knapsack_id_sum(knapsack)
-        if k < NUMBER_OF_NEIGHBORHOODS:
-            x1 = random_neighbour(knapsack, max_weight, knapsack_weight, k, items, restrictions)
-        else:
-            x1 = random_neighbour(knapsack, max_weight, knapsack_weight, 0, items, restrictions)
-        #print ("Entrou")
-        # print("3333333333333")
-        # print knapsack_id_sum(knapsack)
+        # Gera aleatoriamente um x1
+        x1 = random_neighbour(knapsack, max_weight, knapsack_weight, k, items, restrictions)
+
+        # Encontra um maximo local a partir de x1
         x2 = find_local_maximum(x1, max_weight, knapsack_weight, items, restrictions)
-        #print("JORGE")
-        # print knapsack_id_sum(knapsack)
-        #print ("Saiu")
-        # print ("################")
-        # print knapsack_id_sum(x2)
-        # print evaluate_solution(x2)
-        # print knapsack_id_sum(knapsack)
-        # print evaluate_solution(knapsack)
-        # print ("XXXXXXXXXXXXXXXX")
+
+        # Verifica se o maximo local encontrado eh melhor que a solucao ate o momento
         if (evaluate_solution(x2) > evaluate_solution(knapsack)):
+            # Altera a solucao
             knapsack = x2
+            # print "Solucao ate o momento: " + str(evaluate_solution(knapsack))
             knapsack_weight = calculate_knapsack_weight(knapsack)
-            # print "Melhorou com k = " + str(k)
-            # print evaluate_solution(x2)
+            # Volta para a vizinhanca 1
             k = 1
         else:
             k = k + 1
+
+#print_knapsack(knapsack)
 print "Profit: " + str(evaluate_solution(knapsack))
 print "Weight: " + str(calculate_knapsack_weight(knapsack))
-# print_knapsack(knapsack)
--- implementation/vns.py
+++ implementation/vns.py
@@ -1,125 +1,86 @@
 from random import randint
 from random import shuffle
 
+# constantes
 NUM_ITEM = 0
 PROFIT = 1
 WEIGHT = 2
 
-# functions to generate neighborhood
+# numero de vizinhancas
+NUMBER_OF_NEIGHBORHOODS = 3
 
-def neighborhood0(array):
-    return [array, array]
-
-def neighborhood1(array):
-    return [array, array, array]
-
-def neighborhood2(array):
-    return [array, array, array, array]
-
-def random_neighbour(knapsack, max_weight, knapsack_weight, items_to_remove, items, restrictions):
+# funcao para gerar um vizinho aleatorio dada uma vizinhanca
+def random_neighbour(knapsack, max_weight, knapsack_weight, vizinhanca, items, restrictions):
     new_knapsack = knapsack[:]
-    if items_to_remove == 0 or items_to_remove >= len(new_knapsack):
-        #zera a mochila
+    # a ultima vizinhanca gera uma mochila totalmente nova
+    if vizinhanca == NUMBER_OF_NEIGHBORHOODS or vizinhanca >= len(new_knapsack):
         new_knapsack = []
         knapsack_weight = 0
-        #print "Removeu todos itens da mochila"
     else:
-        #remove items_to_remove itens da mochila
-        for i in range(0, items_to_remove):
+        # remove x itens aleatorios da mochila onde x = numero da vizinhanca
+        for i in range(0, vizinhanca):
             index = randint(0,len(new_knapsack) - 1)
             knapsack_weight -= new_knapsack[index][WEIGHT]
-            #print "Removeu item " + str(knapsack[index][NUM_ITEM])
             new_knapsack.pop(index)
 
+    # filtra o array de itens para respeitar as restricoes de peso e conflito
     possible_items = items_that_can_be_chosen(new_knapsack, max_weight, knapsack_weight, items, restrictions)
-    #adiciona todos itens que conseguir
-    #items1
+
+    # insere itens na mochila ate que nenhum item possa ser inserido
     while possible_items != []:
         index = randint(0,len(possible_items) - 1)
         new_knapsack.append(possible_items[index])
         knapsack_weight += possible_items[index][WEIGHT]
         possible_items = items_that_can_be_chosen(new_knapsack, max_weight, knapsack_weight, items, restrictions)
-        #print "Adicionou item " + str(item[NUM_ITEM])
-
-    #items2
-    # shuffle(items)
-    #
-    # for item in items:
-    #     if item_can_be_chosen(new_knapsack, item, restrictions):
-    #         new_knapsack.append(item)
-    #         knapsack_weight += item[WEIGHT]
-
-    #items3
-    # for i in range(0, len(knapsack)):
-    #     i = random_item_max_weight(items, max_weight - knapsack_weight)
-    #     if i is not None:
-    #         if item_can_be_chosen(new_knapsack, i, restrictions):
-    #             if knapsack_has_space(max_weight, knapsack_weight, i):
-    #                 new_knapsack.append(i)
-    #                 knapsack_weight += i[WEIGHT]
-    #                 #print "Adicionou item " + str(item[NUM_ITEM])
-
-    #print knapsack
 
     return new_knapsack
 
-# knapsack functions
+# funcoes auxiliares
+# verifica se um item pode ser escolhido baseado nos conflitos e se ele ja esta na mochila
 def item_can_be_chosen(knapsack, item, restrictions):
     for i in knapsack:
         if restrictions[i[NUM_ITEM]][item[NUM_ITEM]] or i[0] == item[0]:
             return False
     return True
 
+# filtra um array de itens para respeitar as restricoes de peso e conflito
 def items_that_can_be_chosen(knapsack, max_weight, knapsack_weight, items, restrictions):
     return filter(lambda item: item_can_be_chosen(knapsack, item, restrictions) and knapsack_has_space(max_weight, knapsack_weight, item), items)
 
-def random_item_max_weight(items, weight):
-    items_that_fit = filter(lambda (a,b,c): c <= weight, items)
-
-    if len(items_that_fit) > 0:
-        index = randint(0,len(items_that_fit) - 1)
-
-        return items_that_fit[index]
-
-    return None
-
+# verifica se um item cabe na mochila
 def knapsack_has_space(max_weight, knapsack_weight, item):
     return item[WEIGHT] <= (max_weight - knapsack_weight)
 
+# funcao para encontrar um maximo local dada uma solucao (hill climbing)
 def find_local_maximum(initial, max_weight, knapsack_weight, items, restrictions):
     initial_value = evaluate_solution(initial)
 
-    #print "VALOR INICIAL: " + str(initial_value)
     biggest_solution = initial
     biggest_value = initial_value
 
-    # supondo que usaremos o criterio 0 pra achar os vizinhos aqui
     neighborhood = []
 
+    # gera x vizinhos sendo x = max(tamanho da mochila * 2, 100)
+    # para mochilas com menos de 50 itens, gera 100 vizinhos
+    # para mochilas com mais de 50 itens, gera itens*2 vizinhos
     for i in range(0, max(len(initial)*2, 100)):
         vizinho = random_neighbour(initial, max_weight, calculate_knapsack_weight(initial), 1, items, restrictions)
-        #print ("$$$$$$$$")
-        #vizinho.sort(key=lambda (a,b,c): a)
-        #print vizinho
         neighborhood.append(vizinho)
 
+    # avalia os vizinhos gerados
     for n in neighborhood:
         value = evaluate_solution(n)
-        #print value
-        #print "Valor da vizinhanca " + str(value)
 
         if value > biggest_value:
             biggest_value = value
             biggest_solution = n
 
     if biggest_value > initial_value:
-        # print "Recursionou: " + str(biggest_value)
         return find_local_maximum(biggest_solution, max_weight, calculate_knapsack_weight(biggest_solution), items, restrictions)
     else:
-        #print initial_value
         return initial
 
-
+# calcula o lucro de uma mochila
 def evaluate_solution(knapsack):
     value = 0
     for item in knapsack:
@@ -127,6 +88,7 @@ def evaluate_solution(knapsack):
 
     return value
 
+# calcula o peso de uma mochila
 def calculate_knapsack_weight(knapsack):
     weight = 0
     for item in knapsack:
@@ -134,15 +96,7 @@ def calculate_knapsack_weight(knapsack):
 
     return weight
 
-def knapsack_id_sum(knapsack):
-    id_sum = 0
-    for item in knapsack:
-        id_sum += item[NUM_ITEM]
-
-    return id_sum
-
-# other functions
-
+# printa uma mochila
 def print_knapsack(knapsack):
     total_weight = 0
     total_profit = 0
@@ -152,7 +106,3 @@ def print_knapsack(knapsack):
         print "Id: " + str(item[NUM_ITEM]) + " - Peso: " + str(item[WEIGHT]) + " - Lucro: " + str(item[PROFIT])
     print "########################################################"
     print "Peso total: " + str(total_weight) + " - Lucro total: " + str(total_profit)
-
-def random_neighbor(neighborhood):
-    index = randint(0,len(neighborhood) - 1)
-    return neighborhood[index]
